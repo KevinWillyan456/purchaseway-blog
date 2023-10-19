0e35fc001e5d38cb1bd51b45de64c119e6af526e
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-19 17:10:11 -0400

    feat: atualização dos estilos dos ícones

---
+++ src/icons/Photograph.tsx
@@ -0,0 +1,35 @@
+function Photograph() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            viewBox="0 0 26 26"
+            fill="none"
+        >
+            <path
+                d="M8.66667 11.9167C9.86328 11.9167 10.8333 10.9466 10.8333 9.74999C10.8333 8.55338 9.86328 7.58333 8.66667 7.58333C7.47005 7.58333 6.5 8.55338 6.5 9.74999C6.5 10.9466 7.47005 11.9167 8.66667 11.9167Z"
+                stroke="black"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M7.1073 22.75C13.1414 9.64164 18.1572 7.34497 23.8339 15.8492"
+                stroke="black"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M19.5 3.25H6.49996C4.10672 3.25 2.16663 5.1901 2.16663 7.58333V18.4167C2.16663 20.8099 4.10672 22.75 6.49996 22.75H19.5C21.8932 22.75 23.8333 20.8099 23.8333 18.4167V7.58333C23.8333 5.1901 21.8932 3.25 19.5 3.25Z"
+                stroke="black"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+}
+
+export default Photograph;
--- src/icons/User.tsx
+++ src/icons/User.tsx
@@ -10,16 +10,16 @@ function User() {
             <path
                 d="M26.6667 11.6667C26.6667 15.3486 23.6819 18.3333 20 18.3333C16.3181 18.3333 13.3334 15.3486 13.3334 11.6667C13.3334 7.98477 16.3181 5 20 5C23.6819 5 26.6667 7.98477 26.6667 11.6667Z"
                 stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
             <path
                 d="M20 23.3333C13.5567 23.3333 8.33337 28.5567 8.33337 35H31.6667C31.6667 28.5567 26.4434 23.3333 20 23.3333Z"
                 stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
         </svg>
     );
