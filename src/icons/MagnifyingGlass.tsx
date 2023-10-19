3279faf4f8693ccad9990bacc70473d8c888607e
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-19 07:56:10 -0400

    style: conflitos com nomenclatura de estilos

--- src/icons/MagnifyingGlass.tsx
+++ src/icons/MagnifyingGlass.tsx
@@ -10,9 +10,9 @@ function MagnifyingGlass() {
             <path
                 d="M14.4792 14.4935L19.25 19.25M16.5 9.625C16.5 13.4219 13.4219 16.5 9.625 16.5C5.82804 16.5 2.75 13.4219 2.75 9.625C2.75 5.82804 5.82804 2.75 9.625 2.75C13.4219 2.75 16.5 5.82804 16.5 9.625Z"
                 stroke="#026E81"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
         </svg>
     );
--- src/icons/Share.tsx
+++ src/icons/Share.tsx
@@ -11,29 +11,29 @@ function Share() {
             <path
                 d="M33 57C33 65.2842 26.2843 72 18 72C9.71574 72 3 65.2842 3 57C3 48.7158 9.71574 42 18 42C26.2843 42 33 48.7158 33 57Z"
                 stroke="#026E81"
-                stroke-width="6"
+                strokeWidth="6"
             />
             <path
                 d="M63 24L33 45"
                 stroke="#026E81"
-                stroke-width="6"
-                stroke-linecap="round"
+                strokeWidth="6"
+                strokeLinecap="round"
             />
             <path
                 d="M63 90L33 69"
                 stroke="#026E81"
-                stroke-width="6"
-                stroke-linecap="round"
+                strokeWidth="6"
+                strokeLinecap="round"
             />
             <path
                 d="M93 96C93 104.284 86.2842 111 78 111C69.7158 111 63 104.284 63 96C63 87.7158 69.7158 81 78 81C86.2842 81 93 87.7158 93 96Z"
                 stroke="#026E81"
-                stroke-width="6"
+                strokeWidth="6"
             />
             <path
                 d="M93 18C93 26.2843 86.2842 33 78 33C69.7158 33 63 26.2843 63 18C63 9.71574 69.7158 3 78 3C86.2842 3 93 9.71574 93 18Z"
                 stroke="#026E81"
-                stroke-width="6"
+                strokeWidth="6"
             />
         </svg>
     );
