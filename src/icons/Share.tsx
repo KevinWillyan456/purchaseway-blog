8f393d6be6064411608ac83e6abc7919eefd8fa6
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-18 20:25:47 -0400

    feat: adicionando mais ícones para a página início

---
+++ src/icons/Share.tsx
@@ -0,0 +1,41 @@
+function Share() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="96"
+            height="114"
+            viewBox="0 0 96 114"
+            fill="none"
+        >
+            <path
+                d="M33 57C33 65.2842 26.2843 72 18 72C9.71574 72 3 65.2842 3 57C3 48.7158 9.71574 42 18 42C26.2843 42 33 48.7158 33 57Z"
+                stroke="#026E81"
+                stroke-width="6"
+            />
+            <path
+                d="M63 24L33 45"
+                stroke="#026E81"
+                stroke-width="6"
+                stroke-linecap="round"
+            />
+            <path
+                d="M63 90L33 69"
+                stroke="#026E81"
+                stroke-width="6"
+                stroke-linecap="round"
+            />
+            <path
+                d="M93 96C93 104.284 86.2842 111 78 111C69.7158 111 63 104.284 63 96C63 87.7158 69.7158 81 78 81C86.2842 81 93 87.7158 93 96Z"
+                stroke="#026E81"
+                stroke-width="6"
+            />
+            <path
+                d="M93 18C93 26.2843 86.2842 33 78 33C69.7158 33 63 26.2843 63 18C63 9.71574 69.7158 3 78 3C86.2842 3 93 9.71574 93 18Z"
+                stroke="#026E81"
+                stroke-width="6"
+            />
+        </svg>
+    );
+}
+
+export default Share;
