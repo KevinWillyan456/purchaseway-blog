36058b99c48bc2a85cb060678ae039e878016738
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-19 08:06:43 -0400

    feat: criação do footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-logo">
+                <img src="/purchaseway-blog-logo-medium.png" alt="logo" />
+            </div>
+            <div className="copyright">
+                @ 2023 Purchaseway Blog - Todos os diretos reservados
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
