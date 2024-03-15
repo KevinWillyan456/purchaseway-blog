f1df5febc8076f48ed6e7fe02adc760eabdd4b6d
Kevin Souza <kevinwillyansouza@gmail.com>
2024-03-15 16:42:26 -0400

    feat: integração dos posts com o back-end

--- src/components/containerposts/ContainerPosts.tsx
+++ src/components/containerposts/ContainerPosts.tsx
@@ -1,167 +1,82 @@
 import Like from "../../icons/Like";
 import User from "../../icons/User";
+import { useEffect, useState } from "react";
 import "./ContainerPosts.css";
+import axios from "axios";
+
+export interface IPost {
+    _id: string;
+    conteudo: { text: string; urlImg: string };
+    respostas: Array<{
+        _id: string;
+        userId: string;
+        text: string;
+        dataCriacao: Date;
+    }>;
+    proprietario: string;
+    curtidas: number;
+    dataCriacao: Date;
+}
 
 function ContainerPosts() {
+    const [posts, setPosts] = useState<IPost[]>([]);
+
+    useEffect(() => {
+        axios
+            .get(import.meta.env.VITE_API_URL + "/posts", {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: import.meta.env.VITE_API_KEY,
+                },
+            })
+            .then((response) => {
+                setPosts(response.data.posts);
+            });
+    }, []);
+
     return (
         <section className="container-posts">
             <div className="content-posts">
                 <p className="recommendations">Recomendações para você</p>
 
-                <article className="user-posted">
-                    <div className="user-picture-posts">
-                        <User />
-                    </div>
-                    <div className="wrapper">
-                        <div className="name-wrapper">
-                            <div className="name">Joe Dawn</div>
-                            <div className="posted-in">há 2 horas</div>
+                {posts.map((post) => (
+                    <article className="user-posted" key={post._id}>
+                        <div className="user-picture-posts">
+                            <User />
                         </div>
-                        <div className="title-post">
-                            Desenvolvedores Front-end em React
-                        </div>
-                        <div className="content-post">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Optio tenetur dolorum natus voluptates magnam
-                            ipsa delectus qui voluptate, sint ratione reiciendis
-                            adipisci minus magni ducimus
-                        </div>
-                        <div className="actions-post">
-                            <div className="actions-wrapper">
-                                <div className="like-btn-wrapper">
-                                    <button className="like-btn">
-                                        <Like />
-                                    </button>
-                                    <div className="count-like-post">12</div>
-                                </div>
-                                <button className="btn-respond">
-                                    Responder
-                                </button>
+                        <div className="wrapper">
+                            <div className="name-wrapper">
+                                <div className="name">Joe Dawn</div>
+                                <div className="posted-in">há 2 horas</div>
                             </div>
-                            <button className="answers">
-                                Respostas
-                                <div className="answers-count">21</div>
-                            </button>
-                            <article className="user-posted-answering">
-                                <div className="user-picture-posts">
-                                    <User />
-                                </div>
-                                <div className="wrapper">
-                                    <div className="name-wrapper">
-                                        <div className="name">Joe Dawn</div>
-                                        <div className="posted-in">
-                                            há 2 horas
+                            <div className="title-post">Título do post</div>
+                            <div className="content-post">
+                                {post.conteudo.text}
+                            </div>
+                            <div className="actions-post">
+                                <div className="actions-wrapper">
+                                    <div className="like-btn-wrapper">
+                                        <button className="like-btn">
+                                            <Like />
+                                        </button>
+                                        <div className="count-like-post">
+                                            {post.curtidas}
                                         </div>
                                     </div>
-                                    <div className="content-post">
-                                        Lorem ipsum dolor sit, amet consectetur
-                                        adipisicing elit?
-                                    </div>
-                                    <div className="actions-post">
-                                        <div className="actions-wrapper">
-                                            <button className="like-btn">
-                                                <Like />
-                                            </button>
-                                            <div className="count-like-post">
-                                                5
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </article>
-                        </div>
-                    </div>
-                </article>
-                <article className="user-posted">
-                    <div className="user-picture-posts">
-                        <User />
-                    </div>
-                    <div className="wrapper">
-                        <div className="name-wrapper">
-                            <div className="name">Joe Doe</div>
-                            <div className="posted-in">há 2 dias</div>
-                        </div>
-                        <div className="title-post">
-                            Desenvolvedores Back-end em Node
-                        </div>
-                        <div className="content-post">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Optio tenetur dolorum natus voluptates magnam
-                            ipsa delectus qui voluptate, sint ratione reiciendis
-                            adipisci minus magni ducimus fuga temporibus illo
-                            quam distinctio?
-                        </div>
-                        <div className="actions-post">
-                            <div className="actions-wrapper">
-                                <div className="like-btn-wrapper">
-                                    <button className="like-btn active">
-                                        <Like />
+                                    <button className="btn-respond">
+                                        Responder
                                     </button>
-                                    <div className="count-like-post">23</div>
                                 </div>
-                                <button className="btn-respond">
-                                    Responder
+                                <button className="answers">
+                                    Respostas
+                                    <div className="answers-count">
+                                        {post.respostas.length}
+                                    </div>
                                 </button>
                             </div>
-                            <button className="answers">
-                                Respostas
-                                <div className="answers-count">5</div>
-                            </button>
                         </div>
-                    </div>
-                </article>
-                <article className="user-posted">
-                    <div className="user-picture-posts">
-                        <User />
-                    </div>
-                    <div className="wrapper">
-                        <div className="name-wrapper">
-                            <div className="name">Jane Dawn</div>
-                            <div className="posted-in">agora mesmo</div>
-                        </div>
-                        <div className="title-post">
-                            Mahou Shoujo Madoka Magica
-                        </div>
-                        <div className="content-post">
-                            "Mahou Shoujo Madoka Magica" é um anime notável e
-                            inovador no gênero de mahou shoujo (garotas
-                            mágicas). Ele se destaca por sua abordagem sombria e
-                            complexa, subvertendo as expectativas comuns
-                            associadas ao gênero. Uma das características mais
-                            marcantes do anime é a maneira como lida com os
-                            desejos. Ao longo da história, os personagens são
-                            confrontados com a oportunidade de realizar um
-                            desejo, mas a realização desses desejos vem com um
-                            preço significativo. Isso levanta questões profundas
-                            sobre o custo dos nossos desejos e a natureza
-                            complexa do que realmente queremos.
-                            <img
-                                style={{
-                                    width: "100%",
-                                    marginTop: "20px",
-                                }}
-                                src="https://i.pinimg.com/736x/56/56/76/56567651819d5511d03ea4c1c25a6a9d.jpg"
-                            />
-                        </div>
-                        <div className="actions-post">
-                            <div className="actions-wrapper">
-                                <div className="like-btn-wrapper">
-                                    <button className="like-btn active">
-                                        <Like />
-                                    </button>
-                                    <div className="count-like-post">2</div>
-                                </div>
-                                <button className="btn-respond">
-                                    Responder
-                                </button>
-                            </div>
-                            <button className="answers">
-                                Respostas
-                                <div className="answers-count">5</div>
-                            </button>
-                        </div>
-                    </div>
-                </article>
+                    </article>
+                ))}
             </div>
         </section>
     );
