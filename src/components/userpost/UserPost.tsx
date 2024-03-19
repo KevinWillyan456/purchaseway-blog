302210b188f7056100fef0110223f2e9b3032a51
Kevin Souza <kevinwillyansouza@gmail.com>
2024-03-19 17:30:35 -0400

    feat: criação da funcionalidade da fazer postagens

--- src/components/userpost/UserPost.tsx
+++ src/components/userpost/UserPost.tsx
@@ -1,9 +1,60 @@
 import './UserPost.css'
 import Photograph from '../../icons/Photograph'
+import { useContext } from 'react'
+import { UserContext } from '../../contexts/UserContext'
+import axios from 'axios'
 
 function UserPost() {
+    const { user } = useContext(UserContext)
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const message = e.currentTarget.querySelector(
+            '.user-message'
+        ) as HTMLTextAreaElement
+        const title = e.currentTarget.querySelector(
+            '.title-publish'
+        ) as HTMLInputElement
+
+        if (message.value === '') {
+            message.focus()
+            return
+        }
+
+        if (title.value === '') {
+            title.focus()
+            return
+        }
+
+        axios
+            .post(
+                import.meta.env.VITE_API_URL + '/posts/' + user._id,
+                {
+                    title: title.value,
+                    text: message.value,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: import.meta.env.VITE_API_KEY,
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 201) {
+                    message.value = ''
+                    title.value = ''
+
+                    alert('Postagem criada com sucesso!')
+                }
+            })
+            .catch(() => {
+                alert('Erro ao criar postagem!')
+            })
+    }
+
     return (
-        <article className="user-post-container">
+        <form className="user-post-container" onSubmit={handleSubmit}>
             <textarea
                 className="user-message focus-ring"
                 placeholder="No que está pensando?"
@@ -20,9 +71,11 @@ function UserPost() {
                         placeholder="Título da publicação"
                     />
                 </div>
-                <button className="btn-publish">Publicar</button>
+                <button className="btn-publish" type="submit">
+                    Publicar
+                </button>
             </div>
-        </article>
+        </form>
     )
 }
 
