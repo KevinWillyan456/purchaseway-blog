e9db4c6bfa481caf42920686893bc4f6f7fb2c36
Kevin Souza <kevinwillyansouza@gmail.com>
2024-03-19 17:13:12 -0400

    feat: adição da funcionalidade de caso o usuário conectado acesse a página de login ele será redirecionado ao serviço

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react'
 import Header from '../components/header/Header'
 import MainLogin from '../components/mainlogin/MainLogin'
+import authenticate from '../utils/AuthUtils'
 
 function Login() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [requestHasEnded, setRequestHasEnded] = useState(false)
+
+    useEffect(() => {
+        async function checkAuthentication() {
+            const authenticated = await authenticate()
+            setIsAuthenticated(authenticated)
+            setRequestHasEnded(true)
+        }
+        checkAuthentication()
+    }, [])
+
     return (
         <>
-            <Header />
-            <MainLogin />
+            {isAuthenticated
+                ? (window.location.href = '/dashboard')
+                : requestHasEnded && (
+                      <>
+                          <Header />
+                          <MainLogin />
+                      </>
+                  )}
         </>
     )
 }
