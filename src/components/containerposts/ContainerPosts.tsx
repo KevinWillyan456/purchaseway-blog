1445098bc70605af1016d762dbb74a317f8b50ee
Kevin Souza <kevinwillyansouza@gmail.com>
2024-04-11 14:04:26 -0400

    fix: mudança na forma de receber as postagens

--- src/components/containerposts/ContainerPosts.tsx
+++ src/components/containerposts/ContainerPosts.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react'
 import './ContainerPosts.css'
 import axios from 'axios'
 import PostComponent from './postcomponent/PostComponent'
-import { GlobalContext, IPost } from '../../contexts/GlobalContext'
+import { GlobalContext } from '../../contexts/GlobalContext'
 
 function ContainerPosts() {
     const { posts, setPosts } = useContext(GlobalContext)
@@ -10,8 +10,6 @@ function ContainerPosts() {
     const [error, setError] = useState<boolean>(false)
 
     useEffect(() => {
-        const previousPosts: IPost[] = []
-
         axios
             .get(import.meta.env.VITE_API_URL + '/posts', {
                 headers: {
@@ -20,85 +18,11 @@ function ContainerPosts() {
                 },
             })
             .then((response) => {
-                response.data.posts.forEach((post: IPost) => {
-                    previousPosts.push(post)
-                })
-
-                if (response.data.posts.length === 0) {
+                if (response.data.length === 0) {
                     setEmptyPosts(true)
+                } else {
+                    setPosts(response.data.posts)
                 }
-
-                previousPosts.forEach((post) => {
-                    if (!post.proprietario.match(/(\w{8}(-\w{4}){3}-\w{12}?)/g))
-                        return
-
-                    axios
-                        .get(
-                            import.meta.env.VITE_API_URL +
-                                '/users/' +
-                                post.proprietario,
-                            {
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    Authorization: import.meta.env.VITE_API_KEY,
-                                },
-                            }
-                        )
-                        .then((response) => {
-                            previousPosts.forEach((prevPost) => {
-                                if (prevPost._id === post._id) {
-                                    prevPost.proprietario =
-                                        response.data.user.nome
-                                    prevPost.proprietarioId =
-                                        response.data.user._id
-                                }
-                                return prevPost
-                            })
-                        })
-                })
-
-                previousPosts.forEach((post) => {
-                    post.respostas.forEach((answer) => {
-                        if (answer.userName) return
-
-                        axios
-                            .get(
-                                import.meta.env.VITE_API_URL +
-                                    '/users/' +
-                                    answer.userId,
-                                {
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        Authorization: import.meta.env
-                                            .VITE_API_KEY,
-                                    },
-                                }
-                            )
-                            .then((response) => {
-                                previousPosts.forEach((prevPost) => {
-                                    if (prevPost._id === post._id) {
-                                        prevPost.respostas.forEach(
-                                            (prevAnswer) => {
-                                                if (
-                                                    prevAnswer._id ===
-                                                    answer._id
-                                                ) {
-                                                    prevAnswer.userName =
-                                                        response.data.user.nome
-                                                }
-                                                return prevAnswer
-                                            }
-                                        )
-                                    }
-                                    return prevPost
-                                })
-                            })
-                    })
-                })
-
-                setTimeout(() => {
-                    setPosts(previousPosts)
-                }, 1000)
             })
             .catch(() => {
                 setError(true)
