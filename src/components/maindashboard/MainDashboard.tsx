0444f4e524cc49fd94d00039628f1036724b15ad
Kevin Souza <kevinwillyansouza@gmail.com>
2024-04-12 17:23:50 -0400

    feat: criação da funcionalidade de levar o scroll da página para o topo

--- src/components/maindashboard/MainDashboard.tsx
+++ src/components/maindashboard/MainDashboard.tsx
@@ -2,8 +2,32 @@ import './MainDashboard.css'
 import UserContent from '../usercontent/UserContent'
 import UserPost from '../userpost/UserPost'
 import ContainerPosts from '../containerposts/ContainerPosts'
+import Up from '../../icons/Up'
+import { useEffect, useState } from 'react'
 
 function MainDashboard() {
+    const [isVisible, setIsVisible] = useState(false)
+
+    const handleScroll = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
+
+    useEffect(() => {
+        const onScroll = () => {
+            const scrollPosition = window.scrollY
+            setIsVisible(scrollPosition > 300)
+        }
+
+        window.addEventListener('scroll', onScroll)
+
+        return () => {
+            window.removeEventListener('scroll', onScroll)
+        }
+    }, [])
+
     return (
         <main className="main-dashboard">
             <div className="main-dashboard-content">
@@ -13,6 +37,11 @@ function MainDashboard() {
                 </div>
                 <ContainerPosts />
             </div>
+            {isVisible && (
+                <button className="up-button" onClick={handleScroll}>
+                    <Up />
+                </button>
+            )}
         </main>
     )
 }
