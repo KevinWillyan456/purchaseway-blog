b82a57f1e8d2b0abefd03a5cdde4f15e43b348a3
Kevin Souza <kevinwillyansouza@gmail.com>
2024-05-26 19:13:20 -0400

    feat: adição de mensagem de espera durante a verificação de autenticação

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -26,6 +26,49 @@ function Dashboard() {
                     <MainDashboard />
                     <Footer />
                 </GlobalContextProvider>
+            ) : !requestHasEnded ? (
+                <section
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        gap: '16px',
+                        height: '100vh',
+                        fontSize: '24px',
+                        color: 'var(--color-dark)',
+                        maxWidth: '600px',
+                        width: 'calc(100% - 40px)',
+                        margin: '0 auto',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    Aguarde...
+                    <p
+                        style={{
+                            fontSize: '20px',
+                            color: 'var(--color-dark)',
+                            textAlign: 'justify',
+                            fontWeight: 'normal',
+                        }}
+                    >
+                        Estamos verificando se você está autenticado. Caso não
+                        seja redirecionado para a página de login em alguns
+                        segundos,{' '}
+                        <a
+                            href="/login"
+                            style={{
+                                color: 'var(--color-dark)',
+                                textDecoration: 'none',
+                                borderBottom: '1px solid var(--color-dark)',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            clique aqui
+                        </a>
+                        .
+                    </p>
+                </section>
             ) : (
                 requestHasEnded && (window.location.href = '/login')
             )}
